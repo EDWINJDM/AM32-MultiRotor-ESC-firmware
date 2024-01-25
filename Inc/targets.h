595e83f77f417446041e1790d14a1afe5774fcdb
EDWINJDM <46374392+EDWINJDM@users.noreply.github.com>
2024-01-25 11:21:28 -0800

    Update targets.h

--- Inc/targets.h
+++ Inc/targets.h
@@ -17,7 +17,7 @@
 //#define AIKONSINGLE_F051
 //#define FLYCOLOR_F051
 //#define AM32REF_F051
-#define BLPWR_F051
+define BLPWR_F051
 //#define HVFLYCOLOR_F051
 //#define FLASHHOBBY_F051
 //#define SEQURE_G071
